5c1b940f9c9784ea171d42b3e31031e8f516413d
DanielAssis1 <43318923+DanielAssis1@users.noreply.github.com>
2018-10-01 14:27:23 -0300

    att2

--- roteiro4_ex6/Program.cs
+++ roteiro4_ex6/Program.cs
@@ -10,6 +10,19 @@ namespace roteiro4_ex6
     {
         static void Main(string[] args)
         {
+            double valor1 = 5.00, lucroesperado=0, ingressos1 = 120;
+            
+            Console.WriteLine(" |Valor do Ingresso|  |Lucro máximo|  |Qnt. Ingressos Vendidos| ");
+
+            while (valor1 >= 1.00)
+            {
+                lucroesperado = (valor1*ingressos1)-200; 
+
+                Console.WriteLine("        R$" +valor1+"               R$"+lucroesperado+"           "+ingressos1+" ingressos.\n");
+                ingressos1 += 26;
+                valor1 -= 0.50;
+            }
+            Console.ReadKey();
         }
     }
 }
